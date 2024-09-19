b754ecd03c73494e837255faf88c929c7dd4fe27
yunhao0204 <y39hao@gmail.com>
2024-09-19 12:08:56 +0800

    add the ImageInfoProvider

---
+++ Tips/ImageInfoProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Tips
+{
+    public class ImageInfoProvider
+    {
+
+
+        public async Task<string> GetImageInfo(string imagePath)
+        {
+            // Check if the image exists
+            if (!File.Exists(imagePath))
+            {
+                throw new FileNotFoundException("Image not found at the given path.");
+            }
+
+
+            return "mock summary";
+        }
+
+    }
+
+}
